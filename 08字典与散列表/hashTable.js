2b7cf805b3237a61937d1cb2694d751219d53ed5
pancras.zheng <pancras.z@foxmail.com>
2022-06-12 23:21:31 +0800

    处理散列表冲突的方法：分离链接

--- 08字典与散列表/hashTable.js
+++ 08字典与散列表/hashTable.js
@@ -1,6 +1,80 @@
-class HashTable {
+import { defaultToString } from '../06链表/util.js';
+import { ValuePair } from '../models/value-pair.js';
+
+export default class HashTable {
     constructor(toStrFn = defaultToString) {
         this.toStrFn = toStrFn;
         this.table = {};
+    }
+
+    //创建散列函数
+    loseloseHashCode(key) {
+        if (typeof key === 'number') { // {1} 
+            return key;
+        }
+        const tableKey = this.toStrFn(key); // {2} 
+        let hash = 0; // {3} hash 变量来存储总和
+        for (let i = 0; i < tableKey.length; i++) {
+            hash += tableKey.charCodeAt(i); // {4}
+        }
+        return hash % 37; // {5} 使用 hash 值和一个任意数做除法的余数,防止超过数值变量最大表示范围
+    }
+    hashCode(key) {
+        return this.loseloseHashCode(key);
+    }
+
+    //将键和值加入散列表
+    put(key, value) {
+        if (key != null && value != null) { // {1}
+            const position = this.hashCode(key); // {2} 
+            this.table[position] = new ValuePair(key, value); // {3} 
+            return true;
+        }
+        return false;
+    }
+
+    //从散列表中获取一个值
+    get(key) {
+        const valuePair = this.table[this.hashCode(key)];
+        return valuePair == null ? undefined : valuePair.value;
+    }
+
+    //从散列表中移除一个值
+    remove(key) {
+        const hash = this.hashCode(key); // {1} 
+        const valuePair = this.table[hash]; // {2} 在 hash 的位置获取到 valuePair
+        if (valuePair != null) {
+            delete this.table[hash]; // {3} 
+            return true;
+        }
+        return false;
+    }
+
+    getTable() {
+        return this.table;
+    }
+
+    isEmpty() {
+        return this.size() === 0;
+    }
+
+    size() {
+        return Object.keys(this.table).length;
+    }
+
+    clear() {
+        this.table = {};
+    }
+
+    toString() {
+        if (this.isEmpty()) {
+            return '';
+        }
+        const keys = Object.keys(this.table);
+        let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`;
+        for (let i = 1; i < keys.length; i++) {
+            objString = `${objString},{${keys[i]} => ${this.table[keys[i]].toString()}}`;
+        }
+        return objString;
     }
 }
--- 08字典与散列表/index.js
+++ 08字典与散列表/index.js
@@ -1,5 +1,7 @@
 import Dictionary from "./dictionary.js";
+import HashTable from "./hashTable.js";
 
+console.log('\n--------使用Dictionary类---------');
 const dictionary = new Dictionary();
 dictionary.set('Gandalf', 'gandalf@email.com');
 dictionary.set('John', 'johnsnow@email.com');
@@ -10,3 +12,35 @@ console.log(dictionary.keys());
 console.log(dictionary.values());
 console.log(dictionary.get('Tyrion'));
 console.log(dictionary.keyValues());
+
+console.log('\n--------使用HashTable类---------');
+const hash = new HashTable();
+hash.put('Gandalf', 'gandalf@email.com');
+hash.put('John', 'johnsnow@email.com');
+hash.put('Tyrion', 'tyrion@email.com');
+console.log(hash.hashCode('Gandalf') + ' - Gandalf');
+console.log(hash.hashCode('John') + ' - John');
+console.log(hash.hashCode('Tyrion') + ' - Tyrion');
+
+console.log(hash.get('Gandalf')); // gandalf@email.com 
+console.log(hash.get('Loiane')); // undefined
+
+hash.remove('Gandalf');
+console.log(hash.get('Gandalf')); // undefined
+
+console.log('\n---------散列表中的冲突-----------');
+const hashc = new HashTable();
+hashc.put('Ygritte', 'ygritte@email.com');
+hashc.put('Jonathan', 'jonathan@email.com');
+hashc.put('Jamie', 'jamie@email.com');
+hashc.put('Jack', 'jack@email.com');
+hashc.put('Jasmine', 'jasmine@email.com');
+hashc.put('Jake', 'jake@email.com');
+hashc.put('Nathan', 'nathan@email.com');
+hashc.put('Athelstan', 'athelstan@email.com');
+hashc.put('Sue', 'sue@email.com');
+hashc.put('Aethelwulf', 'aethelwulf@email.com');
+hashc.put('Sargeras', 'sargeras@email.com');
+console.log(hashc.toString())
+
+console.log('\n---------处理冲突-----------');
